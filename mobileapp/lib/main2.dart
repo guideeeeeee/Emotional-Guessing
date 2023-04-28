f967cc7c0baf73317e90f3f8de969f7958cd2ccf
guideeeeeee <pratom5108@gmail.com>
2023-04-28 22:59:16 +0700

    font change

--- mobileapp/lib/main2.dart
+++ mobileapp/lib/main2.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:mobileapp/user.dart';
 
@@ -31,15 +33,26 @@ class attention extends StatefulWidget {
   @override
   State<attention> createState() => _attentionState();
 }
-  
+
 class _attentionState extends State<attention> {
-  bool _visible = true;
+  @override
+  void initState() {
+    super.initState();
+    for(var i = 0;i<2;i++){
+    Timer.periodic(Duration(seconds: 5), (timer) { 
+      setState(() {
+        _visible = !_visible;
+      });
+    });}
+  }
+  bool _visible = false;
   @override
   Widget build(BuildContext context) {
     return AnimatedOpacity(opacity: _visible ? 1.0 : 0.0,
-     duration: const Duration(seconds: 3),
+     duration: const Duration(seconds: 1),
      child: Center(
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text("คำแนะนำการใช้แอพพลิเคชัน",style: TextStyle(fontSize: 22)),
           Text("ผู้ใหญ่ควรเล่นปล่อยให้เด็กงงกับโค้ดต่อไปในมื้อเช้าของเราที่จะเก็บในลาวาของคอมพิวเตอร์คีย์บอร์ดนาโนที่สูงใหญ่และเสียดสีแรงกล้าสุดแกร่ง",style: TextStyle(fontSize: 18),)
