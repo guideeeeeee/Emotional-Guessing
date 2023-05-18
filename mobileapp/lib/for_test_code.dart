b25ab2cccc4ea4c43cd22041e072da8831459703
guideeeeeee <pratom5108@gmail.com>
2023-05-18 14:30:46 +0700

    wait for debug

--- mobileapp/lib/for_test_code.dart
+++ mobileapp/lib/for_test_code.dart
@@ -1,7 +1,80 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 import 'package:mobileapp/game/gameover.dart';
+import 'package:flutter/material.dart';
+import 'package:mobileapp/game/game.dart';
 void main() {
-  runApp(gameOver());
+  runApp(game());
 }
 
+class game extends StatefulWidget {
+  const game({Key? key});
+
+  @override
+  State<game> createState() => _gameState();
+}
+
+class _gameState extends State<game> {
+  int timeLeft = 5;
+
+  @override
+  void initState() {
+    super.initState();
+    startCountDown();
+  }
+
+  void startCountDown() {
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      if (timeLeft > 0) {
+        setState(() {
+          timeLeft--;
+        });
+      } else {
+        timer.cancel();
+        _gameOver();
+      }
+    });
+  }
+
+  void _gameOver() {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => gameOver()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Builder(
+          builder: (context) => Stack(
+            children: [
+              Container(
+                width: double.infinity,
+                height: double.infinity,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/background.jpg"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: Center(
+                  child: Text(
+                    timeLeft.toString(),
+                    style: TextStyle(
+                      fontFamily: 'FC Lamoon',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 22,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
 
--- mobileapp/lib/game/game.dart
+++ mobileapp/lib/game/game.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:mobileapp/game/gameover.dart';
 
 class game extends StatefulWidget {
   const game({super.key});
@@ -9,10 +11,60 @@ class game extends StatefulWidget {
 
 //รอนายมาช่วยทำ
 class _gameState extends State<game> {
+  int timeLeft = 5;
+  void StartCountDown() {
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      if (timeLeft > 0) {
+        setState(() {
+          timeLeft--;
+        });
+      } else {
+        timer.cancel();
+        _gameOver();
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    StartCountDown();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Builder(
+          builder: (context) => Stack(
+            children: [
+              Container(
+                width: double.infinity,
+                height: double.infinity,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/background.jpg"),
+                        fit: BoxFit.cover)),
+                child: Center(
+                    child: Text(
+                  timeLeft.toString(),
+                  style: TextStyle(
+                      fontFamily: 'FC Lamoon',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 22),
+                )),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+  void _gameOver() {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => gameOver()),
     );
   }
 }
